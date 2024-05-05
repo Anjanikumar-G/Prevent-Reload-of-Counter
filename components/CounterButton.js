f98376fe3d93f6071ddc4cf8dbf91632e8c618d5
Anjanikumar Guruvali <108091230+Anjanikumar-G@users.noreply.github.com>
2024-05-05 13:40:25 +0530

    Update CounterButton.js

--- components/CounterButton.js
+++ components/CounterButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function CounterButton() {
      /*DO NOT EDIT THIS LINE */ console.log("CounterButton rendered!"); //DO NOT edit this line 
-
+ return <button onClick={increment}>Increment</button>;
 }
 
-export default /*___________*/
+export default React.memo(CounterButton);
